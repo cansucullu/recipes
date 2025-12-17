3af2245c84df151cd803d7b8af3c74930dfdf07b
Can Sücüllü <cansucullu@gmail.com>
2025-12-17 23:45:21 +0000

    Add docstrings and fix import order in venn_single.py

--- venn_diagrams/venn_single.py
+++ venn_diagrams/venn_single.py
@@ -1,12 +1,36 @@
-from matplotlib_venn import venn2
+"""
+Draws Venn Diagrams with 2 sets and an intersection
+"""
+
 import matplotlib.pyplot as plt
+from matplotlib_venn import venn2
 
 
 FONT_SIZE = 18
 ALPHA = 0.7
 
 
-def draw_venn_single(area1, area2, savefig=False, name="venn_single_example"):
+def draw_venn_single(area1, area2, savefig=False,
+                     name="venn_single_example"):
+    """
+    Draw vectors.
+
+    Parameters
+    ----------
+    area1 : int
+        Releative area of the first set (set on the left)
+    area2 : int
+        Releative area of the second set (set on the right)
+    savefig : bool
+        Boolean flag to save the fig, default is False
+    name : str
+        Name of the file when saved. Redundant if savefig=False.
+        Default is venn_single_example.
+
+    Returns
+    -------
+    None
+    """
     v = venn2(subsets=(area1, area2, 0.5), set_labels=('', '', ''))
 
     v.get_patch_by_id('10').set_alpha(ALPHA)
@@ -26,8 +50,8 @@ def draw_venn_single(area1, area2, savefig=False, name="venn_single_example"):
     v.get_label_by_id('11').set_text('?')
     v.get_label_by_id('11').set_fontsize(FONT_SIZE)
 
-    c1 = v.centers[0][0]
-    c2 = v.centers[1][0]
+    c1 = v.centers[0].asarray()[0]
+    c2 = v.centers[1].asarray()[0]
     r1 = v.radii[0] / 2
     r2 = v.radii[1] / 2
     m = 0 if area1 == area2 else (-1 if area1 > area2 else 1)
